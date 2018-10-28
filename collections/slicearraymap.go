4377969f2d7b2c87f4434f2dbd874705f8561c98
precompiler <lishengyin.msn@hotmail.com>
2018-10-28 20:50:27 +0000

    add array demo

---
+++ collections/slicearraymap.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+	var integers [5]int
+	fmt.Println(integers)
+	integers[1] = 1
+	fmt.Println(integers)
+	strings := [3]string{"Hello", "World", "!"}
+	for _, item := range strings {
+		fmt.Print(item)
+	}
+	fmt.Println()
+	fmt.Println([...]int{1, 2, 3})
+	fmt.Println([]int{0: 10, 8: 20})
+	fmt.Println([10]int{0: 10, 8: 20})
+
+	words := [5]*string{4: new(string)}
+	fmt.Println(words)
+	a, b := "a", "b"
+	words[0] = &a
+	words[1] = &b
+	fmt.Println(words)
+	fmt.Println(*words[0])
+
+	colors1 := []string{"red", "green", "blue"}
+	colors2 := colors1
+	colors4 := [3]string{}
+	colors3 := [3]string{"red", "green", "blue"}
+	colors4 = colors3
+	colors2[0] = "black"
+	fmt.Println(colors1[0], colors2[0])
+	colors4[0] = "white"
+	fmt.Println(colors3[0], colors4[0])
+
+	slice := make([]string, 5)
+	fmt.Println(slice)
+
+	num1 := []int{1, 2, 3}
+	var num2 []int
+	num2 = num1
+	addr1 := &num1[0]
+	addr2 := &num2[0]
+	fmt.Println(&num2, &num1, addr1, addr2)
+
+}
