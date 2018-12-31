9b5a14223ec970b5ad173cd258d1ebb8837daac7
precompiler <lishengyin.msn@hotmail.com>
2018-12-31 11:19:03 +0000

    Add method demo

--- udt/demo.go
+++ udt/demo.go
@@ -16,6 +16,12 @@ func main() {
 	user3 := User{Name: "Scott Tiger", Email: "scott.tiger@oracle.com", IsAdmin: true}
 	fmt.Println(user2)
 	fmt.Println(user3)
+	rect := Rectangle{}
+	rect.SetWidth(10)
+	rect.SetLength(20)
+	fmt.Println(rect.Area())
+	rect.dummy(0)
+	fmt.Println(rect.Area())
 }
 
 type User struct {
@@ -26,3 +32,24 @@ type User struct {
 
 type Meter float64
 type Foot float64
+
+type Rectangle struct {
+	width  float32
+	length float32
+}
+
+func (r Rectangle) Area() float32 {
+	return r.length * r.width
+}
+
+func (r *Rectangle) SetWidth(width float32) {
+	r.width = width
+}
+
+func (r *Rectangle) SetLength(length float32) {
+	r.length = length
+}
+
+func (r Rectangle) dummy(length float32) {
+	r.length = length //mutating a copy
+}
